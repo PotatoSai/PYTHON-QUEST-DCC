62095f257a93d249a63ed0af407b951d97351d88
PotatoSai <141911781+PotatoSai@users.noreply.github.com>
2024-02-19 17:31:35 +0800

    Update hypotenuse_calculator.py

--- day-04/TASKS/hypotenuse_calculator.py
+++ day-04/TASKS/hypotenuse_calculator.py
@@ -3,7 +3,11 @@ import math
 # Write the code ↓ to read the lengths of the two shorter sides of a right-angled triangle from the user.
 # Be cautious when reading input of various data types.
 
+print("HYPOTENUSE CALCULATOR FOR ALF")
 
+sideA = float(input("Enter the length of side A: "))
+sideB = float(input("Enter the length of side B: "))
+total = 0.0
 
 
 
@@ -14,9 +18,14 @@ import math
 
 
 
+total = math.sqrt(math.pow(sideA, 2) + math.pow(sideB, 2))
+
+
 
 
 # Write the code ↓ to display the calculated hypotenuse.
+
+print("The hypothenuse of the right-angled triangle is: %.2f" % total)
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
