10f568a0aa85da69c3ca796c063f17564cdf10d7
PotatoSai <141911781+PotatoSai@users.noreply.github.com>
2024-02-19 16:28:16 +0800

    Update simple_calculator.py

--- day-02/TASKS/simple_calculator.py
+++ day-02/TASKS/simple_calculator.py
@@ -2,7 +2,12 @@
 # NOTE: The two operands must be read as floats.
 
 
+print("SIMPLE CALCULATOR ALF")
 
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+total = 0.0
+operator = input("Enter the operator (+, -, x, /): ").lower()
 
 
 
@@ -12,6 +17,16 @@
 # Write the code ↓ to perform the calculations based on the selected operation.
 
 
+if operator == "+":
+    total = first + second
+elif operator == "-":
+    total = first - second
+elif operator == "x":
+    total = first * second
+elif operator == "/":
+    total = first / second
+else:
+    print("\nInvalid operator.")
 
 
 
@@ -19,6 +34,8 @@
 
  
 # Write the code ↓ to display the result.
+
+print("\nThe result of %.1f %s %.1f is %.1f" % (first, operator, second, total))
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
